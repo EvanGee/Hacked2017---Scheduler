3aa7ae5879318608dfe86ac0dfe37c88566d8da1
Evan Giese <giesee@mymacewan.ca>
2017-01-29 04:50:33 -0700

    I closed the one file that i was working on so i decied to commit stuff

--- src/client/components/Calender/Calender.js
+++ src/client/components/Calender/Calender.js
@@ -8,12 +8,14 @@ var Data = ["1:00", "2:00", "3:00", "4:00", "5:00", "6:00", "7:00", "8:00", "9:0
             , "13:00", "14:00", "15:00", "16:00", "17:00", "18:00", "19:00", "20:00", "21:00", "22:00", "23:00", "24:00"];
 
 var Calender = React.createClass({
-  
+  send: function (e) {
+    console.log(this.props);
+  },
 
   render: function () {
 
     return (
-
+      <div>
       <table>
         {WeekDays.map(function (d) {
           return <DayName day={d} /> ;
@@ -26,7 +28,8 @@ var Calender = React.createClass({
               );
         })}
       </table>
-      <Button></button>
+      <button onClick={this.send}> SEND </button>
+      </div>
     );
   }
 
